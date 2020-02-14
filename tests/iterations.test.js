f305932d7ba3cbbbdc7153964ade785a0a122239
King Elisha <ek.chibueze@gmail.com>
2020-02-14 17:19:53 +0100

    added tests for negative integers

--- tests/iterations.test.js
+++ tests/iterations.test.js
@@ -10,4 +10,14 @@ describe('Binary Gap Unit Tests', () => {
     expect(solution(1000000000000000)).toEqual(3)
     expect(solution(2000000)).toEqual(4)
   })
+
+  it('should return correct binary gap for a negative integer', () => {
+    expect(solution(-29383829)).toEqual(7)
+    expect(solution(-10228372)).toEqual(5)
+    expect(solution(-2828)).toEqual(4)
+    expect(solution(-615242)).toEqual(3)
+    expect(solution(-10000)).toEqual(3)
+    expect(solution(-1000000000000000)).toEqual(3)
+    expect(solution(-2000000)).toEqual(4)
+  })
 })
