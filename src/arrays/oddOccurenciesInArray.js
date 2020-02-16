073ea0dfa62de443b6588b0ead0df7b979043045
King Elisha <ek.chibueze@gmail.com>
2020-02-16 20:52:49 +0100

    optimized implementation

--- src/arrays/oddOccurenciesInArray.js
+++ src/arrays/oddOccurenciesInArray.js
@@ -1,28 +1,14 @@
 /**
- * 
+ * Returns the value of the unpaired element
+ * Detected time complexity: O(N) or O(N*log(N))
  * @param {number[]} A 
  */
 function solution(A) {
-  const odd = []
-  for (let i = 0; i < A.length; i++) {
-    const idx = odd.indexOf(A[i])
-    idx === -1 ? odd.push(A[i]) : odd.splice(idx, 1)
+  A.sort((a, b) => a - b)
+  for (let i = 0; i < A.length; i += 2) {
+    if (A[i] !== A[i + 1])
+      return A[i]
   }
-
-  return odd[0]
 }
 
-function generateRandomIntArray(N, x0, x1) {
-  const arr = []
-
-  for (let i = 0; i < N; i++) {
-    arr[i] = Math.round(x0 + Math.random() * x1)
-  }
-
-  return arr
-}
-
-const y = generateRandomIntArray(1000000, 0, 100000)
-let t0 = Date.now()
-const odd = solution(y)
-console.log(`odd: ${odd} time: ${(Date.now() - t0) / 1000}`)
+module.exports = solution
