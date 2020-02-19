e65b6eca975602aee7379c9964b70cc7a7aee9c6
King Elisha <ek.chibueze@gmail.com>
2020-02-19 12:45:05 +0100

    implemented solution

---
+++ src/countingElements/maxCounters.js
@@ -0,0 +1,28 @@
+
+/**
+ * 
+ * @param {number} N 
+ * @param {number[]} A 
+ */
+function solution(N, A) {
+  let counters = new Array(N).fill(0)
+  let max = 0
+  let upper = 0
+
+  for (let i = 0; i < A.length; i++) {
+    const k = A[i] - 1
+
+    if (k + 1 === N + 1)
+      upper = max
+    else {
+      counters[k] = Math.max(counters[k] + 1, upper + 1)
+
+      if (counters[k] > max)
+        max = counters[k]
+    }
+  }
+
+  counters = counters.map(n => Math.max(n, upper))
+
+  return counters
+}
