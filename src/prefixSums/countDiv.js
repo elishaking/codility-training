65f0c83ac1e38326f67e6b3c5be63cd6af77a1c3
King Elisha <ek.chibueze@gmail.com>
2020-02-20 08:40:57 +0100

    implemented best solution

--- src/prefixSums/countDiv.js
+++ src/prefixSums/countDiv.js
@@ -5,20 +5,7 @@
  * @param {number} K 
  */
 function solution(A, B, K) {
-  let divisibleCount = 0
-  let delta = 1
+  let val = Math.floor((B / K)) - Math.floor((A / K))
 
-  for (let i = A; i <= B; i += delta) {
-    if (i % K === 0) {
-      divisibleCount++
-      delta = K
-    }
-  }
-
-  return divisibleCount
+  return val + ((A % K === 0) ? 1 : 0)
 }
-
-const t0 = Date.now()
-// const y = solution(6, 11, 2)
-const y = solution(0, 2000000000, 1)
-console.log(`val: ${y} time: ${(Date.now() - t0) / 1000}`)
