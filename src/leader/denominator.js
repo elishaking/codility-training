8ac0f64bd540e701ad1c627fb5c5e26941ee35e4
King Elisha <ek.chibueze@gmail.com>
2020-03-01 12:23:37 +0100

    updated docs

--- src/leader/denominator.js
+++ src/leader/denominator.js
@@ -1,5 +1,8 @@
 /**
+ * Given an array A consisting of N integers, returns index of any
+ * element of array A in which the dominator of A occurs
  *
+ * Detected time complexity: O(N*log(N)) or O(N)
  * @param {number[]} A
  */
 function solution(A) {
