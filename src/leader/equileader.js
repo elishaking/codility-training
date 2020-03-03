94d1fb0292a3fca12bcabb95a947e717154bc5e4
King Elisha <ek.chibueze@gmail.com>
2020-03-03 02:38:25 +0100

    updated docs

--- src/leader/equileader.js
+++ src/leader/equileader.js
@@ -1,5 +1,13 @@
 /**
+ * A non-empty array A consisting of N integers is given.
+ * The leader of this array is the value that occurs in more than half of the elements of A.
  *
+ * An equi leader is an index S such that 0 ≤ S < N − 1 and two sequences
+ * A[0], A[1], ..., A[S] and A[S + 1], A[S + 2], ..., A[N − 1] have leaders of the same value
+ *
+ * Given a non-empty array A consisting of N integers, returns the number of equi leaders.
+ *
+ * Detected time complexity: O(N)
  * @param {number[]} A
  */
 function solution(A) {
