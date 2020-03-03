8c25d8808a3a6d8dc1340b29a0e7142ae10139ac
King Elisha <ek.chibueze@gmail.com>
2020-03-03 02:36:02 +0100

    implemented solution

--- src/leader/equileader.js
+++ src/leader/equileader.js
@@ -3,5 +3,42 @@
  * @param {number[]} A
  */
 function solution(A) {
-  // todo
+  let aLeader = false,
+    leader = A[0];
+  const leaders = {};
+
+  if (A.length < 2) return 0;
+
+  for (let i = 0; i < A.length; i++) {
+    const num = A[i];
+    if (leaders[num]) {
+      leaders[num][1]++;
+
+      if (leaders[num][1] > A.length / 2) {
+        aLeader = true;
+        leader = A[i];
+      }
+    } else {
+      leaders[num] = [i, 1];
+    }
+  }
+
+  if (!aLeader) return 0;
+
+  let l = 0,
+    k = leaders[leader][1];
+  let nEquiLeaders = 0;
+
+  for (let i = 0; i < A.length; i++) {
+    if (A[i] === leader) {
+      l++;
+      k--;
+    }
+
+    if (l > (i + 1) / 2 && k > (A.length - i - 1) / 2) nEquiLeaders++;
+  }
+
+  return nEquiLeaders;
 }
+
+console.log(solution([4, 3, 4, 4, 4, 2]));
