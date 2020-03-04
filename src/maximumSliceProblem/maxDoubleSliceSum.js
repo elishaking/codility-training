bcd7643caf5e79aacd3201b75387c987ad5fbd5a
King Elisha <ek.chibueze@gmail.com>
2020-03-04 08:48:47 +0100

    updated docs

--- src/maximumSliceProblem/maxDoubleSliceSum.js
+++ src/maximumSliceProblem/maxDoubleSliceSum.js
@@ -1,5 +1,13 @@
 /**
+ * A non-empty array A consisting of N integers is given.
+ * A triplet (X, Y, Z), such that 0 ≤ X < Y < Z < N, is called
+ * a double slice. The sum of double slice (X, Y, Z) is the total
+ * of A[X + 1] + A[X + 2] + ... + A[Y − 1] + A[Y + 1] + A[Y + 2] + ... + A[Z − 1]
  *
+ * Given a non-empty array A consisting of N integers, returns
+ * the maximal sum of any double slice
+ *
+ * Detected time complexity: O(N)
  * @param {number[]} A
  */
 function solution(A) {
