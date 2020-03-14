a315fba301175a378803c895da337a3ba78c3805
King Elisha <ek.chibueze@gmail.com>
2020-03-14 16:21:04 +0100

    second solution

--- src/primeAndCompositeNumbers/minPerimeterRectangle.js
+++ src/primeAndCompositeNumbers/minPerimeterRectangle.js
@@ -3,7 +3,13 @@
  * @param {number} N
  */
 function solution(N) {
-  return Math.round((4 * N) / Math.sqrt(N));
+  let minPerimeter = Number.MAX_SAFE_INTEGER;
+
+  for (let i = 0; i < Math.sqrt(N); i++) {
+    if (N % i === 0) minPerimeter = Math.min(minPerimeter, 2 * (N / i + i));
+  }
+
+  return minPerimeter;
 }
 
 console.log(solution(30));
