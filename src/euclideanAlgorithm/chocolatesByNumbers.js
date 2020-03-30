0f706b7b31728bd9785785a49e9670f6e3352f11
King Elisha <ek.chibueze@gmail.com>
2020-03-30 08:34:20 +0100

    updated docs

--- src/euclideanAlgorithm/chocolatesByNumbers.js
+++ src/euclideanAlgorithm/chocolatesByNumbers.js
@@ -1,4 +1,17 @@
 /**
+ * Two positive integers N and M are given. Integer N represents the number of chocolates
+ * arranged in a circle, numbered from 0 to N − 1.
+ *
+ * You start to eat the chocolates. After eating a chocolate you leave only a wrapper.
+ *
+ * You begin with eating chocolate number 0. Then you omit the next M − 1 chocolates
+ * or wrappers on the circle, and eat the following one.
+ *
+ * You stop eating when you encounter an empty wrapper.
+ *
+ * Given two positive integers N and M, returns the number of chocolates that you will eat.
+ *
+ * Detected time complexity: O(log(N + M))
  *
  * @param {number} N
  * @param {number} M
@@ -12,7 +25,5 @@ function solution(N, M) {
 
   return N / gcd(N, M);
 }
-
-console.log(solution(100, 10));
 
 module.exports = solution;
