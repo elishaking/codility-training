eb7f4706c6ca3378b1faa1dd4c68a4b4c32b2d7d
King Elisha <ek.chibueze@gmail.com>
2020-03-30 09:54:40 +0100

    implemented 2nd solution

--- src/euclideanAlgorithm/commonPrimeDivisors.js
+++ src/euclideanAlgorithm/commonPrimeDivisors.js
@@ -4,35 +4,52 @@
  * @param {number[]} B
  */
 function solution(A, B) {
-  const primeDivisors = N => {
-    const primeNumbersSieve = new Array(N + 1).fill(true);
-    primeNumbersSieve[0] = primeNumbersSieve[1] = false;
+  /**
+   * Calculates greatest common divisor
+   * @param {number} a
+   * @param {number} b
+   */
+  const gcd = (a, b) => {
+    if (a % b === 0) return b;
 
-    // sieve
-    for (let i = 2; i * i <= N; i++) {
-      if (primeNumbersSieve[i]) {
-        let k = i * i;
-        while (k <= N) {
-          primeNumbersSieve[k] = false;
-          k += i;
-        }
-      }
-    }
-
-    const divisors = [];
-    for (let i = 2; i < primeNumbersSieve.length; i++) {
-      if (primeNumbersSieve[i] && N % i == 0) divisors.push(i);
-    }
-
-    return JSON.stringify(divisors);
+    return gcd(b, a % b);
   };
 
-  let kPositions = 0;
+  /**
+   * Checks if the two numbers have the same prime divisors
+   * @param {number} a
+   * @param {number} b
+   */
+  const commonDivisors = (a, b) => {
+    if (a === 0 && b === 0) return true;
+
+    const g = gcd(a, b);
+
+    while (a !== 1) {
+      const ag = gcd(a, g);
+
+      if (ag === 1) return false;
+
+      a /= ag;
+    }
+
+    while (b !== 1) {
+      const bg = gcd(b, g);
+
+      if (bg === 1) return false;
+
+      b /= bg;
+    }
+
+    return true;
+  };
+
+  let KPositions = 0;
   for (let i = 0; i < A.length; i++) {
-    if (primeDivisors(A[i]) === primeDivisors(B[i])) kPositions++;
+    if (commonDivisors(A[i], B[i])) KPositions++;
   }
 
-  return kPositions;
+  return KPositions;
 }
 
 console.log(solution([15, 75], [75, 15]));
